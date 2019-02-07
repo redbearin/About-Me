66c543ababe2ad86806daaa398c8e8b65fb372e3
redbearin <perrigoj@bellsouth.net>
2019-02-06 19:51:15 -0800

    Added stylesheet and updated index and js

--- js/app.js
+++ js/app.js
@@ -1,66 +1,79 @@
-//'use strict';
-
-//'use strict';
+'use strict';
 
 console.log('Information about Me');
 
-// Ask the viewer my favorite color. If correct color is selected, notify user. If not, ask user to pick again.
-var runway = prompt('Do you think I ever landed on an ice runway? ').toLowerCase();
+// Ask the viewer if I landed on an ice runway and provide responses based on yes/no or y/n answer.
+var runway = prompt('Do you think I ever landed on an ice runway? Yes or No ').toLowerCase();
 if (runway === 'yes') {
   console.log('Ice runway guess: ' + runway);
-  alert ('Yes, I frequently landed on an ice runways when I worked on construction projects north of the Arctic Circle in the winter months.');
+  alert ('Yes, I frequently landed on an ice runways when I worked north of the Arctic Circle.');
+}
+if (runway === 'y') {
+  console.log('Ice runway guess: ' + runway);
+  alert ('Yes, I frequently landed on an ice runways when I worked north of the Arctic Circle.');
 }
 else {
-  alert ('Good guess, but the answer is yes! When I worked north on construction projects in the winter months, we frequnetly had to land on ice runways.');
+  alert ('Good guess, but the answer is yes. When I worked north of the Arctic Circle, we frequnetly had to land on ice runways.');
   console.log('Ice runway guess: ' + runway);
 }
 
-/*
-// Ask the viewer my favorite sport. If correct sport is selected, notify user. If not, ask user to pick again.
-var favSport = prompt('What is my favorite sport? ');
-while (favSport !== 'soccer') {
-  console.log('Favorite sport: ' + favSport);
-  alert ('That is not my favorite sport. Please try again. Please use lowercase letters.');
-  favSport = prompt('What is my favorite sport? ');
+// Ask the viewer whether I live in the desert and provide responses based on yes/no or y/n answer.
+var desert = prompt('Do I live in the desert? Yes or No').toLowerCase();
+if (desert === 'no') {
+  console.log('Desert guess: ' + desert);
+  alert ('You are correct. I live in the Sierras near Lake Tahoe, so there are a lot of big pine trees.');
 }
-if (favSport === 'soccer') {
-  alert ('You have great taste in athletic events! Yes, ' + favSport + ' is my favorite sport.');
-  console.log('Favorite sport: ' + favSport);
+if (desert === 'n') {
+  console.log('Desert guess: ' + desert);
+  alert ('You are correct. I live in the Sierras near Lake Tahoe, so there are a lot of big pine trees.');
 }
-
-// Ask the viewer my favorite restaurant. If correct restaurant is selected, notify user. If not, ask user to pick again.
-var favRestaurant = prompt('What is my favorite restaurant? ');
-while (favRestaurant !== 'slanted door') {
-  console.log('Favorite restaurant: ' + favRestaurant);
-  alert ('That is not my favorite restaurant. My favorite restaurant is in San Francisco. I has one Michelin star. It is located at the Ferry Building. The name have two word (s------ and d---). Please try again. Please use lowercase letters.');
-  favRestaurant = prompt('What is my favorite restaurant? ');
-}
-if (favRestaurant === 'slanted door') {
-  alert ('You are definitely a foodie! Yes, the ' + favRestaurant + ' is my favorite restaurant.');
-  console.log('Favorite restaurant: ' + favRestaurant);
+else {
+  alert ('Good guess, but the answer is no! I live in the Sierras near Lake Tahoe.');
+  console.log('Desert guess: ' + desert);
 }
 
-// Ask the viewer my favorite coffee shop. If correct coffee shop is selected, notify user. If not, ask user to pick again.
-var favCoffee = prompt('What is my favorite coffee shop? ');
-while (favCoffee !== 'ancient grounds') {
-  console.log('Favorite coffee shop: ' + favCoffee);
-  alert ('That is not my favorite coffee shop. It is in Seattle. The name is two words (a----- g-------). It has ancient artifacts and wonderful coffee. It is downtown. Please try again. Please make sure to use lower case letters.');
-  favCoffee = prompt('What is my favorite coffee shop? ');
+// Ask the viewer how I feel about biological and provide responses based on yes/no or y/n answer.
+var synBiology = prompt('Do you think I have an interest in progamming cells (synthetic biology/biological computing)? Yes or No').toLowerCase();
+if (synBiology === 'yes') {
+  console.log('Desert guess: ' + synBiology);
+  alert ('You are correct, I am fascinated with the potential of biological computing.');
 }
-if (favCoffee === 'ancient grounds') {
-  alert ('You must like coffee with character! Yes, ' + favCoffee+ ' is my favorite place to get coffee.');
-  console.log('Favorite Color: ' + favCoffee);
+if (synBiology === 'y') {
+  console.log('Desert guess: ' + synBiology);
+  alert ('You are correct, I am fascinated with the potential of biological computing.');
+}
+else {
+  alert ('Good guess, but biological computer a big interest of mine.');
+  console.log('Desert guess: ' + synBiology);
 }
 
-//Ask the viewer my favorite pet. If correct pet is selected, notify user. If not, ask user to pick again.
-var favPet = prompt('What is my favorite pet? ');
-while (favPet !== 'dog') {
-  console.log('Favorite pet: ' + favPet);
-  alert ('That is not my favorite pet. Please try again. Please use lowercase letters.');
-  favPet = prompt('What is my favorite pet? ');
+// Ask the viewer whether I like to eat at Cracker Barrel and provide responses based on yes/no or y/n answer.
+var greasySpoon = prompt('Do I like to eat at Cracker Barrel? Yes or No').toLowerCase();
+if (greasySpoon === 'no') {
+  console.log('Cracker Barrel guess: ' + greasySpoon);
+  alert ('You are correct. Cracker Barrel is one of the least favorite restaurants.');
 }
-if (favPet === 'dog') {
-  alert ('You must like canines! Yes, my favorite pet is the ' + favPet + ' .');
-  console.log('Favorite pet: ' + favPet);
+if (greasySpoon === 'n') {
+  console.log('Cracker Barrel guess: ' + greasySpoon);
+  alert ('You are correct. Cracker Barrel is one of the least favorite restaurants.');
 }
-*/
+else {
+  alert ('Since I lived in Florida for a long time, it is a good assumption that I like to eat at Cracker Barrel. Actually I never adapted to Southern food, so it is one of my least favorite places to dine.');
+  console.log('Cracker Barrel guess: ' + greasySpoon);
+}
+
+// Ask the viewer whether I like yoga and provide responses based on yes or no answer.
+var yoga = prompt('Do I like yoga? Yes or No').toLowerCase();
+if (yoga === 'yes') {
+  console.log('Yoga guess: ' + yoga);
+  alert ('You are correct. I find power yoga a good way to workout and therapeutic yoga a way to decompress.');
+}
+if (yoga === 'y') {
+  console.log('Yoga guess: ' + yoga);
+  alert ('You are correct. I find power yoga a good way to workout and therapeutic yoga a way to decompress.');
+}
+else {
+  alert ('Good guess! Many people think I am a runner, but I prefer yoga over foot races.');
+  console.log('Yoga guess: ' + yoga);
+}
+
